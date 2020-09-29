4220c23ac81317deb5bdbe276dfaae5f99c12ae8
Jack Zhang <jack.zhang@aspiraconnect.com>
2020-09-29 13:43:54 +0800

    ch6

--- ch5-blog-api/posts/views.py
+++ ch5-blog-api/posts/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
-
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -11,5 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
